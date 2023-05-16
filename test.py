8add63bd5a8c102395f343c8254b5f9cfa6db28a
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2023-05-16 10:38:22 -0500

    adding address (only consider entire value for now)

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-import re
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
 
-test = re.sub("[^0-9]", "", "Republica Dominicana, Casado/A Rd3951262")
+def find_nth_right(haystack, needle, n):
+    start = haystack.rfind(needle)
+    while start >= 0 and n > 1:
+        start = haystack.rfind(needle, 0, start-len(needle))
+        n -= 1
+    return start
 
-print(test)
+print(find_nth("Praeba", "a", 1))
+print(find_nth_right("Praeba", "a", 2))
