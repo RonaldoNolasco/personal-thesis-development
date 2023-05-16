c95c9c2a86d4ca541ed35ac2fba402e514f804e0
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2023-05-16 11:25:53 -0500

    first try queue

--- test.py
+++ test.py
@@ -1,16 +1,2 @@
-def find_nth(haystack, needle, n):
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start+len(needle))
-        n -= 1
-    return start
-
-def find_nth_right(haystack, needle, n):
-    start = haystack.rfind(needle)
-    while start >= 0 and n > 1:
-        start = haystack.rfind(needle, 0, start-len(needle))
-        n -= 1
-    return start
-
-print(find_nth("Praeba", "a", 1))
-print(find_nth_right("Praeba", "a", 2))
+if "":
+    print("Ga")
