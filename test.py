e70a3fdd2a995c8a9809f5e7fa99a945af960ef3
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2023-05-21 04:12:29 -0500

    final algorithm to get all 10225 candidates

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(int("5"))
+arr1 = [', Graduado, Terciario/Tecnico.']
+arr2 = ['Ingeniero De Sistemas', ', Graduado, Universitario.']
+arr3 = ['Project Management Professional (Pmp)', '', ', Graduado, Otro.']
+
+arr = [arr1, arr2, arr3]
+for elem in arr:
+    second = elem[-1]
+    print(second)
+    first = "".join(elem[0:-1])
+    print(first)
