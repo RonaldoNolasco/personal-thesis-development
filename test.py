c296f23f6c8eae448149cde5fbb39f036e75c944
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2023-05-22 02:27:44 -0500

    processing with new float type values

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-arr1 = [', Graduado, Terciario/Tecnico.']
-arr2 = ['Ingeniero De Sistemas', ', Graduado, Universitario.']
-arr3 = ['Project Management Professional (Pmp)', '', ', Graduado, Otro.']
-
-arr = [arr1, arr2, arr3]
-for elem in arr:
-    second = elem[-1]
-    print(second)
-    first = "".join(elem[0:-1])
-    print(first)
+print(round(1.4986301369863013, 1))
